2ed5e01127296955ebc5efe133e363e11d4468fd
Mona Lisa <Mona Lisa>
2026-03-16 22:14:51 +0900

    nvim: 補完の自動選択をオフ

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+      },
+    },
+  },
+}
