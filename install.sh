9f9410a585bd1c299bbb623f157a53c6dc6b8f2b
Mona Lisa <Mona Lisa>
2026-04-14 22:44:41 +0900

    fix: ~を$HOMEに
    ~は""の中では展開されない

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "$HOME/dotfiles/bash/.bashrc" "$HOME/.bashrc"
+ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
-mkdir -p ~/.config
-ln -sf ~/dotfiles/nvim ~/.config/nvim
+mkdir -p "$HOME/.config"
+ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
