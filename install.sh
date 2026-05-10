c4da0c8fb4907cda4092e2b1bb46773ece129966
debugon <d>
2026-05-10 23:37:27 +0900

    fix: シンボリックリンクの貼り方を修正
    nオプションでフォルダ作成を抑制
    冪等性

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ ln -sf "$HOME/dotfiles/bash/.bashrc" "$HOME/.bashrc"
 ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 mkdir -p "$HOME/.config"
-ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
-ln -sf "$HOME/dotfiles/wezterm" "$HOME/.config/wezterm"
+ln -sfn "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+ln -sfn "$HOME/dotfiles/wezterm" "$HOME/.config/wezterm"
