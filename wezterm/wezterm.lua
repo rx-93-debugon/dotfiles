5b46a3767251d6d7cdab343358b7aece64ffc0c1
a <a>
2026-05-11 16:37:51 +0900

    fix: リガチャを無効化
    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("UDEV Gothic NFLG")
+config.font = wezterm.font({ family = "UDEV Gothic NFLG", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
 config.font_size = 16.0
 config.line_height = 0.9
 config.line_height = 0.9
