982e351c4c4be64dee7bc6417fb49dc9c3fb59b4
Mona Lisa <Mona Lisa>
2026-06-02 14:49:08 +0900

    fix: 既存ファイルを削除してシンボリックファイルの作成

--- install.sh
+++ install.sh
@@ -4,5 +4,17 @@ ln -sf "$HOME/dotfiles/bash/.bashrc" "$HOME/.bashrc"
 ln -sf "$HOME/dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 mkdir -p "$HOME/.config"
-ln -sfn "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
-ln -sfn "$HOME/dotfiles/wezterm" "$HOME/.config/wezterm"
+
+link_config() {
+  local src="$1"
+  local dest="$2"
+
+  if [ -e "$dest" ] && [ ! -L "$dest" ]; then
+    rm -rf "$dest"
+  fi
+  ln -sfn "$src" "$dest"
+}
+
+link_config "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+link_config "$HOME/dotfiles/wezterm" "$HOME/.config/wezterm"
+link_config "$HOME/dotfiles/lazygit" "$HOME/.config/lazygit"
