a058ddabf82b51c446470b3d8e8b4a25a4076559
ryzen5_7600 <Mona Lisa>
2026-09-20 23:32:32 +0900

    chore: 重複削除
    wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,6 @@ local config = wezterm.config_builder()
 config.font = wezterm.font({ family = "UDEV Gothic NFLG", harfbuzz_features = { "calt=0", "clig=0", "liga=0" } })
 config.font_size = 16.0
 config.line_height = 0.9
-config.line_height = 0.9
 config.color_scheme = "tokyonight"
 config.window_background_opacity = 0.9
 
