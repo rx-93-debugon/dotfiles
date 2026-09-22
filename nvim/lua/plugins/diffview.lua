54734826cb718d971acd2a468b313c56463cc3f2
ryzen5_7600 <Mona Lisa>
2026-09-22 13:59:07 +0900

    feat: <leader>gdにDiffviewのトグル表示キーマップを追加

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -4,5 +4,19 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    keys = {
+      {
+        "<leader>gd",
+        function()
+          if next(require("diffview.lib").views) == nil then
+            vim.cmd("DiffviewOpen")
+          else
+            vim.cmd("DiffviewClose")
+          end
+        end,
+        desc = "Toggle Diffview (side-by-side)",
+      },
+    },
   },
 }
